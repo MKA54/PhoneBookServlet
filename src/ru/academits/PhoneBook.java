e63935c3c36a49d609f46d0823cfb8b33f5ec9ad
Mka91 <kiwogens@yandex.ru>
2021-10-24 15:50:09 +0700

    phoneBook

--- src/ru/academits/PhoneBook.java
+++ src/ru/academits/PhoneBook.java
@@ -6,12 +6,8 @@ import ru.academits.dao.ContactDao;
 import ru.academits.service.ContactService;
 
 public class PhoneBook {
-
     public static ContactDao contactDao = new ContactDao();
-
     public static ContactService phoneBookService = new ContactService();
-
     public static ContactConverter contactConverter = new ContactConverter();
-
     public static ContactValidationConverter contactValidationConverter = new ContactValidationConverter();
 }
